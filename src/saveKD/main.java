214c5d1f72e22938f8a918746f5882df0ab5da7d
angelless <dongkyu91622@gmail.com>
2016-09-20 03:14:55 +0900

    config #1

--- src/saveKD/main.java
+++ src/saveKD/main.java
@@ -1,6 +1,16 @@
 package saveKD
 
 public class main extends PluginBase implements Listener {
+HashMap<String,LinkedTreeMap> userKD;
+public void onEnable(){
+Config config = new Config(this.getDataForder()+"/config.json",Config.JSON);
+for(String key : config.getKeys()){
+if(config.get(key) instanceof LinkedTreeMap<>()){
 
+}
+}
+this.userKD = 
+
+}
 
 }
