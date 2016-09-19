2809c1c22f1437ca919f3fa06783e896c3924050
angelless <dongkyu91622@gmail.com>
2016-09-20 03:16:10 +0900

    #1

--- src/saveKD/main.java
+++ src/saveKD/main.java
@@ -1,7 +1,8 @@
 package saveKD
 
 public class main extends PluginBase implements Listener {
-HashMap<String,LinkedTreeMap> userKD;
+HashMap<String,HashMap> userKD;
+//HashMap
 public void onEnable(){
 Config config = new Config(this.getDataForder()+"/config.json",Config.JSON);
 for(String key : config.getKeys()){
